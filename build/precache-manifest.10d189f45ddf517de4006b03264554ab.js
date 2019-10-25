a11f0bd89184cf7a6c404e2a80fab29395a64ea0
rsoroka <rsoroa@luxoft.com>
2019-10-25 23:57:06 +0300

    Add build

---
+++ build/precache-manifest.10d189f45ddf517de4006b03264554ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39da341661242d1ab239c5e6e6e91680",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61d2cfefce7706059713",
+    "url": "/static/css/main.f7b7d857.chunk.css"
+  },
+  {
+    "revision": "dd524dfa9dc0ce6ec480",
+    "url": "/static/js/2.221ad553.chunk.js"
+  },
+  {
+    "revision": "61d2cfefce7706059713",
+    "url": "/static/js/main.e1dceec3.chunk.js"
+  },
+  {
+    "revision": "a659c513b913be4e7b37",
+    "url": "/static/js/runtime-main.d52357dd.js"
+  },
+  {
+    "revision": "994edca5b95ec7e8e9bbae0793252081",
+    "url": "/static/media/alien.994edca5.png"
+  }
+]);
