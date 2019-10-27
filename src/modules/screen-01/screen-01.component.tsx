509eb43d6a3aece00da1aa7af18a680ee7c03a9f
Roman <r40a90@gmail.com>
2019-10-27 13:43:44 +0300

    Fix screen-01

--- src/modules/screen-01/screen-01.component.tsx
+++ src/modules/screen-01/screen-01.component.tsx
@@ -3,6 +3,8 @@ import { Panel } from '../../components/panel/panel.compoentt';
 import { Page } from '../../components/page/page';
 import panelImage from './images/panel-image.jpg';
 
+import css from './screen-01.module.css';
+
 export class Screen01 extends React.PureComponent {
     handlePrevButtonClick = () => {
         console.log('Prev button clicked!');
@@ -15,13 +17,15 @@ export class Screen01 extends React.PureComponent {
     render() {
         return (
             <Page alienText={'Some text jshd sdfsdf sdf sdfsdf sdf s dfs df '}>
-                <Panel
-                    topText="There is no way I can defeat them if I insert bullets into my gun one by one"
-                    bottomText="You wil need to create a function..."
-                    src={panelImage}
-                    onPrevButtonClick={this.handlePrevButtonClick}
-                    onNextButtonClick={this.handleNextButtonClick}
-                />
+                <div className={css.panelContainer}>
+                    <Panel
+                        topText="There is no way I can defeat them if I insert bullets into my gun one by one"
+                        bottomText="You wil need to create a function..."
+                        src={panelImage}
+                        onPrevButtonClick={this.handlePrevButtonClick}
+                        onNextButtonClick={this.handleNextButtonClick}
+                    />
+                </div>
             </Page>
         );
     }
